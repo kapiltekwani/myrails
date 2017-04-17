c736f9f9a3211f605f11bd86f141948baba638e8
kapil tekwani <kapiltekwani@gmail.com>
2017-04-17 04:57:39 +0000

    made some more code refactoring

--- app/models/report.rb
+++ app/models/report.rb
@@ -19,8 +19,8 @@ class Report
  
       if current_object_state.valid?
         previous_object_state = ObjectState.where(object_id: row["object_id"], object_type: row["object_type"]).order_by(:timestamp => 'desc').first
-        row["object_changes"] = (JSON.parse(previous_object_state.object_changes).merge(JSON.parse(row["object_changes"]))).to_json if previous_object_state
-        current_object_state = ObjectState.create(object_id: row["object_id"], object_type: row["object_type"], timestamp: DateTime.strptime(row["timestamp"],'%s'), object_changes: row["object_changes"])
+        current_object_state.object_changes = (JSON.parse(previous_object_state.object_changes).merge(JSON.parse(row["object_changes"]))).to_json if previous_object_state
+        current_object_state.save 
       else
         #we can use collect invalid rows and send them as another attachment back to user.
         logger.debug "==#{row}"
